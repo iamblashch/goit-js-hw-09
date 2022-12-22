7981b05b5a709b56a1109a28d12cf049e55c8460
iamblashch <bblashchuk08@gmail.com>
2022-12-22 20:41:01 +0200

    js-9

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,46 @@
+import '../css/common.css';
 
+const refs = {
+    bodyEl: document.querySelector('body'),
+    startEl: document.querySelector('button[data-start]'),
+    stopEl: document.querySelector('button[data-stop]'),
+}
+
+refs.stopEl.disabled = true;
+let intervalID = null;
+
+const randomBodyColorGenerator = {
+  DELAY: 100,
+
+  getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  },
+
+  interval() {
+    intervalID = setInterval(() => {
+      changeBgColorRandom();
+    }, this.DELAY);
+    refs.stopEl.disabled = false;
+  },
+
+  start() {
+    refs.startEl.addEventListener('click', () => {
+      this.interval();
+      refs.startEl.disabled = true;
+      refs.stopEl.disabled = false;
+    });
+    refs.stopEl.addEventListener('click', this.stop);
+  },
+
+  stop() {
+    clearInterval(intervalID);
+    refs.stopEl.disabled = true;
+    refs.startEl.disabled = false;
+  },
+};
+
+function changeBgColorRandom() {
+  refs.bodyEl.style.backgroundColor = `${randomBodyColorGenerator.getRandomHexColor()}`;
+}
+
+randomBodyColorGenerator.start();
